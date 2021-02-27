edfb6db16b204e73d59d10b47416abe5a7135e26
handal95 <45425838+handal95@users.noreply.github.com>
2021-02-28 02:44:24 +0900

    add launcher

---
+++ launcher.py
@@ -0,0 +1,28 @@
+import time
+from apscheduler.schedulers.background import BackgroundScheduler
+sched = BackgroundScheduler()
+
+
+def job1():
+    print(f'  job1 : 2 초 마다 실행  {time.strftime("%H:%M:%S")}')
+
+
+def job2():
+    print(f'  job2 : 3 초 마다 실행  {time.strftime("%H:%M:%S")}')
+
+
+def job3():
+    print(f'  job3 : 5 초 마다 실행  {time.strftime("%H:%M:%S")}')
+
+
+sched.add_job(job1, 'cron', second='*/2', id="test_1")
+sched.add_job(job2, 'cron', second='*/3', id="test_2")
+sched.add_job(job3, 'cron', second='*/5', id="test_3")
+
+
+print(f'Scheduling Start {time.strftime("%H:%M:%S")}')
+sched.start()
+print(f'Scheduling done  {time.strftime("%H:%M:%S")}')
+
+while True:
+    time.sleep(1)
