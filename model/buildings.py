13f7039257c8c36719f9d2f0b311088ba1702d09
handal95 <45425838+handal95@users.noreply.github.com>
2021-03-14 23:28:04 +0900

    alembic test

--- model/buildings.py
+++ model/buildings.py
@@ -1,3 +1,5 @@
+import sqlalchemy as sa
+from sqlalchemy import orm
 from dataclasses import dataclass
 
 
@@ -17,3 +19,15 @@ class Building:
     # window_side : str
     # trading_price_min : int
     # trading_price_max : int
+
+
+@dataclass
+class BuildingTemplateModel:
+    __tablename__ = "building_template"
+
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.String(200))
+    category = sa.Column(sa.String(100))
+    traiding_num = sa.Column(sa.Integer)
+    tenant_num = sa.Column(sa.Integer)
+    wolse_num = sa.Column(sa.Integer)
