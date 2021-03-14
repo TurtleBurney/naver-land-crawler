7a1d9f156bef9b34a985db7d176c0a4415ac7db8
YubeenSon <s49451392@gmail.com>
2021-03-14 21:30:58 +0900

    edit variable ahyun to building_in_dong

--- app/crawler.py
+++ app/crawler.py
@@ -1,40 +1,34 @@
 import re
 
-from model.buildings import Building
+from model.buildings import Building_general
 from selenium.webdriver.common.by import By
 
 
 def crawl_general_data(driver):
-    ahyun = []
+    building_in_dong = []
 
     # 북아현동 아파트, 오피스텔 개괄적인 정보 저장
     for i, item in enumerate(driver.find_elements(By.CLASS_NAME, "result_item")):
         info = item.text.split()
-        ahyun.append(info)
+        building_in_dong.append(info)
 
     general_data = []
 
-    for i in range(len(ahyun)):
-        type_trading = ahyun[i][1].split("매매")
-        tenant_wolse = re.split("전세|월세", ahyun[i][2])[1:]
-        b = Building(
-            building_name=ahyun[i][0],
+    for i in range(len(building_in_dong)):
+        type_trading = building_in_dong[i][1].split("매매")
+        tenant_wolse = re.split("전세|월세", building_in_dong[i][2])[1:]
+        b = Building_general(
+            building_name=building_in_dong[i][0],
             building_type=type_trading[0],
-            # contract_type=,
             trading_num=type_trading[1],
             tenant_num=tenant_wolse[0],
             wolse_num=tenant_wolse[1],
-            # building_dong=,
-            # exclusive_area=,
-            # shared_area=,
-            # target_floor=,
-            # max_floor=,
-            # window_side=,
-            # trading_price_min=,
-            # trading_price_max=
         )
         print(b)
         general_data.append(b)
+
+
+# def crawl_specific_data():
 
 
 if __name__ == "__main__":
