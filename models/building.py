19371d97b23bb2d937d4af71153f6c01bccba861
handal95 <45425838+handal95@users.noreply.github.com>
2021-03-23 02:26:06 +0900

    fix precise comment

--- models/building.py
+++ models/building.py
@@ -44,16 +44,16 @@ class Building(Model, BaseModel):
         sa.Enum(BuildingCategory, name="en_building_category"), nullable=False
     )  # 건축물 종류
 
-    built_year = sa.Column(sa.Integer())  # 사용승인일 ? 건축년도?
+    built_year = sa.Column(sa.Integer())  # 사용승인일(건축년도)
     total_dong = sa.Column(sa.Integer())  # 전체 동수
     total_household = sa.Column(sa.Integer())  # 총 세대수
 
     land_address = sa.String(128)  # 지번 주소
     road_address = sa.String(128)  # 도로명 주소
 
-    deal_count = sa.Integer()  # 현재 매매 매물
-    tnant_count = sa.Integer()  # 현재 전세 매물
-    rent_count = sa.Integer()  # 현재 월세 매물
+    deal_count = sa.Integer()  # 현재 매물(매매)의 수
+    tnant_count = sa.Integer()  # 현재 매물(전세)의 수
+    rent_count = sa.Integer()  # 현재 매물(월세)의 수
 
     # relationship
     area_type = orm.relationship("AreaType", backref=orm.backref("area_type"))
