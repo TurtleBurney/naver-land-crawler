47621005cff1248fc03c8e3bc40188d6263a029d
handal95 <45425838+handal95@users.noreply.github.com>
2021-03-23 01:31:59 +0900

    change file name

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,14 @@
 from .base import Base, BaseModel
-from .buildings import Building
+from .building import Building
+from .areatype import AreaType
+from .household import Household
 from .contract import Contract
-from .province import Province
 
 __all__ = [
     Base,
     BaseModel,
+    Building,
+    AreaType,
+    Household,
     Contract,
-    Province,
 ]
