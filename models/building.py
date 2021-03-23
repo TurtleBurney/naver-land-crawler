fca019898dd3cc7c78b3e5edc66e6e4515a97095
handal95 <45425838+handal95@users.noreply.github.com>
2021-03-23 11:51:00 +0900

    building hotfix

--- models/building.py
+++ models/building.py
@@ -6,6 +6,9 @@ from dataclasses import dataclass
 from .base import Model, BaseModel
 
 
+mapper_registry = registry()
+
+
 class BuildingCategory(enum.Enum):
     APARTMENT = "APT"
     OFFICETEL = "OPT"
@@ -22,7 +25,7 @@ class Building(Model, BaseModel):
         category        : Enum                      FALSE
         built_year      : Integer
         total_dong      : Integer
-        total_houshold  : Integer
+        total_household : Integer
         land_address    : String(128)
         road_address    : String(128)
         deal_count      : Integer
@@ -34,26 +37,40 @@ class Building(Model, BaseModel):
     """
 
     # metadata
-    __tablename__ = "building"
+    __table__ = sa.Table(
+        "building",
+        mapper_registry.metadata,
+        sa.Column("id", sa.Integer(), primary_key=True),
+        sa.Column("name", sa.String(256), nullable=False, index=True),  # 건축물 명
+        sa.Column(
+            "category",
+            sa.Enum(BuildingCategory, name="en_building_category"),
+            nullable=False,
+        ),  # 건축물 종류
+        sa.Column("built_year", sa.Integer()),  # 사용승인일(건축년도)
+        sa.Column("total_dong", sa.Integer()),  # 전체 동수
+        sa.Column("total_household", sa.Integer()),  # 총 세대수
+        sa.Column("land_address", sa.String(128)),  # 지번 주소
+        sa.Column("road_address", sa.String(128)),  # 도로명 주소
+        sa.Column("deal_count", sa.Integer()),  # 현재 매물(매매)의 수
+        sa.Column("tnant_count", sa.Integer()),  # 현재 매물(전세)의 수
+        sa.Column("rent_count", sa.Integer()),  # 현재 매물(월세)의 수
+    )
 
     # fields
-    id = sa.Column(sa.Integer(), primary_key=True)
+    id: int
+    name: str
+    category: BuildingCategory
+    built_year: int = None
+    total_dong: int = None
+    total_household: int = None
 
-    name = sa.Column(sa.String(256), nullable=False, index=True)  # 건축물 명
-    category = sa.Column(
-        sa.Enum(BuildingCategory, name="en_building_category"), nullable=False
-    )  # 건축물 종류
+    land_address: str = None
+    road_address: str = None
 
-    built_year = sa.Column(sa.Integer())  # 사용승인일(건축년도)
-    total_dong = sa.Column(sa.Integer())  # 전체 동수
-    total_household = sa.Column(sa.Integer())  # 총 세대수
-
-    land_address = sa.String(128)  # 지번 주소
-    road_address = sa.String(128)  # 도로명 주소
-
-    deal_count = sa.Integer()  # 현재 매물(매매)의 수
-    tnant_count = sa.Integer()  # 현재 매물(전세)의 수
-    rent_count = sa.Integer()  # 현재 매물(월세)의 수
+    deal_count: int = None
+    tnant_count: int = None
+    rent_count: int = None
 
     # relationship
     area_type = orm.relationship("AreaType", backref=orm.backref("area_type"))
