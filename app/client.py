d786e29ae68bb8a5469423724a8a8043e61e1fcb
YubeenSon <s49451392@gmail.com>
2021-03-26 22:37:55 +0900

    divide and clear xpath

--- app/client.py
+++ app/client.py
@@ -5,71 +5,17 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 
-def click_void(xpath, driver):
-    WebDriverWait(driver, 20).until(
-        EC.element_to_be_clickable((By.XPATH, f"{xpath}"))
+def click_void(driver, tag):
+    WebDriverWait(driver, 50).until(
+        EC.element_to_be_clickable((By.XPATH, f"{tag}"))
     ).click()
 
 
-def select_sigungu(driver):
-    # 주소 입력(시, 구, 동)
-    BASE_TAG = "//*[@id='mapSearch']/div[2]/div[1]/section/div[1]/"
-    # 시/도 선택에서 '서울시' 선택
-    click_void(BASE_TAG + "div[1]/div[2]/div/div/table/tbody/tr[1]/td[1]/a", driver)
-    # 시/군/구 선택에서 '서대문구' 선택
-    click_void(BASE_TAG + "div[2]/div[2]/div/div/table/tbody/tr[5]/td[2]/a", driver)
-    # 읍/면/동 선택에서 '북아현동' 선택
-    click_void(BASE_TAG + "div[3]/div[2]/div/div/table/tbody/tr[3]/td[2]/a", driver)
-    driver.implicitly_wait(20)
-    return driver
-
-
-def move_to_page(url):
+def init_driver(url):
     driver = webdriver.Chrome(os.path.join("libs", "chromedriver.exe"))
     driver.get(url)
-
-    apt_btn = driver.find_element_by_class_name(
-        "home_main_button.first_line._rletTypeBtn._innerLink"
-    )
-    apt_btn.click()
-    # 주소 변경 탭 활성화
-    click_void("//*[@id='mapSearch']/div[2]/div[3]/div/div[2]/div[1]/a", driver)
-    select_sigungu(driver)
-    return driver
-
-
-def move_to_each_page(driver, i):
-    # i번째 건물 선택
-    click_void(
-        f"//*[@id='mapSearch']/div[2]/div[1]/section/div[1]/div[4]/div/div[2]/div/ul/li[{(i+1)}]/a",
-        driver,
-    )
-    # 지도로 보기 클릭
-    click_void("//*[@id='mapSearch']/div[2]/div[1]/section/div[2]/a", driver)
-    # 건물 정보 상세보기
-    click_void("//*[@id='_listContainer']/div/div[3]/div/div/div[1]/a", driver)
-    return driver
-
-
-def move_back(driver):
-    driver.back()
-    driver.back()
-
-    BASE_TAG = "//*[@id='mapSearch']/div[2]/"
-
-    # 전체 건물리스트로 재접근을 위해 주소탭 클릭
-    click_void(BASE_TAG + "div[3]/div/div[2]/div[2]/a", driver)
-    # 주소탭 초기화
-    click_void(
-        BASE_TAG + "div[1]/section/div[1]/div[3]/div[1]/div/a[1]",
-        driver,
-    )
-    # 시군구 주소 입력
-    driver = select_sigungu(driver)
-    # 지도에서 보기 클릭
-    click_void(BASE_TAG + "div[1]/section/div[2]/a", driver)
     return driver
 
 
 if __name__ == "__main__":
-    move_to_page()
+    init_driver()
--- app/crawler.py
+++ app/crawler.py
@@ -4,56 +4,62 @@ from models.building import Building
 from selenium.webdriver.common.by import By
 
 
-def crawl_general_data(driver):
-    building_in_dong = []
-
+def crawl_buildings(driver):
     # 북아현동 아파트, 오피스텔 개괄적인 정보 저장
-    for i, item in enumerate(driver.find_elements(By.CLASS_NAME, "result_item")):
-        info = item.text.split()
-        building_in_dong.append(info)
+    building_list = []
+    items = driver.find_elements(By.CLASS_NAME, "result_item")
+    for item in items:
+        building_info = item.text.split()
+        building_name = building_info[0]
 
-    general_data = []
+        # TODO : elements tag로 찾기
+        category = building_info[1].split("매매")[0]
+        deal_count = building_info[1].split("매매")[1]
+        tnant_count = re.split("전세|월세", building_info[2])[1]
+        rent_count = re.split("전세|월세", building_info[2])[2]
 
-    for i in range(len(building_in_dong)):
-        type_and_deal = building_in_dong[i][1].split("매매")
-        tnant_and_rent = re.split("전세|월세", building_in_dong[i][2])[1:]
-        general_data.append(
-            Building(
-                name=building_in_dong[i][0],
-                category=type_and_deal[0],
-                deal_count=type_and_deal[1],
-                tnant_count=tnant_and_rent[0],
-                rent_count=tnant_and_rent[1],
-            )
+        building = Building(
+            name=building_name,
+            category=category,
+            deal_count=deal_count,
+            tnant_count=tnant_count,
+            rent_count=rent_count,
         )
+        building_list.append(building)
+    
+    return building_list
 
-    return driver, general_data
 
-
-def crawl_specific_data(driver, general_data, i):
+def crawl_households(driver, building):
+    # TODO : naming problem... dong_household...
     ID_TAG = "//*[@id='_basic_content_cd']/"
 
-    built_year = driver.find_element_by_xpath(
-        ID_TAG + "article[1]/div[2]/div/div[2]/div[2]/span[2]"
-    ).text
-    raw_dong_and_hosehold = driver.find_element_by_xpath(
-        ID_TAG + "article[1]/div[2]/div/div[1]/div[1]/span[2]"
-    ).text
+    BUILDING_INFO_TAG = f"{ID_TAG}article[1]/div[2]/div/"
+
+    dong_household_tag = f"{BUILDING_INFO_TAG}div[1]/div[1]/span[2]"
+    built_year_tag = f"{BUILDING_INFO_TAG}div[2]/div[2]/span[2]"
+
+    built_year = driver.find_element_by_xpath(built_year_tag).text
+    dong_hosehold_string = driver.find_element_by_xpath(dong_household_tag).text
+    # 예외 발생(1,910세대(임대 326세대 포함, 총22동)) << 이의 경우 out of range 오류 발생
+    dong_household = re.split("세대|동", dong_hosehold_string)
+
     land_address = driver.find_element_by_xpath(ID_TAG + "article[4]/div[2]/p[1]").text
     road_address = driver.find_element_by_xpath(ID_TAG + "article[4]/div[2]/p[2]").text
-    # 예외 발생(1,910세대(임대 326세대 포함, 총22동)) << 이의 경우 out of range 오류 발생
-    total_dong_and_household = re.split(
-        "세대|동", raw_dong_and_hosehold
-    )  # 괄호 지우기는 방법이 이상적이지만 실패
 
-    general_data[i].built_year = built_year
-    general_data[i].total_dong = total_dong_and_household[1][1:]
-    general_data[i].total_household = total_dong_and_household[0]
-    general_data[i].land_address = land_address
-    general_data[i].road_address = road_address[8:]
+    building.built_year = built_year
+    building.total_household = dong_household[0]
 
-    return driver
+    building.land_address = land_address
+    building.road_address = road_address[8:]
+
+    # TODO : 조건문으로 예외처리하기
+    building.total_dong = dong_household[1][1:]
 
 
 if __name__ == "__main__":
-    crawl_general_data()
+    crawl_buildings()
+
+
+# 100세대(1동)
+# 1,910세대(임대 326세대 포함, 총22동)
