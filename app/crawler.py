de04d9c74589ac0ec802fe1c1417c990102edeea
YubeenSon <s49451392@gmail.com>
2021-03-28 18:12:48 +0900

    edit try statement, crawlable_list, for_sale

--- app/crawler.py
+++ app/crawler.py
@@ -7,16 +7,17 @@ from selenium.webdriver.common.by import By
 def get_building_count(driver):
     quantities = driver.find_elements_by_class_name("quantity")
     crawlable_list = []
-    count, for_sale = 0, 0
-    for building in quantities:
+    count, property_count = 0, 0
+    for i, building in enumerate(quantities):
         if count < 2:
-            for_sale = for_sale + int(building.text)
+            property_count = property_count + int(building.text)
             count += 1
         else:
-            for_sale = for_sale + int(building.text)
-            crawlable = True if for_sale > 0 else False
-            crawlable_list.append(crawlable)
-            count, for_sale = 0, 0
+            property_count = property_count + int(building.text)
+            crawlable = True if property_count > 0 else False
+            if crawlable is True:
+                crawlable_list.append(index)
+            count, property_count = 0, 0
     return len(quantities) // 3, crawlable_list
 
 
@@ -31,20 +32,18 @@ def crawl_buildings(driver):
     land_address = driver.find_element_by_class_name("p_address_place._addr").text
     road_address = driver.find_element_by_class_name("p_address_road._road_addr").text[8:]
     category = driver.find_element_by_class_name("label_detail.label_detail--positive").text[:-3]
+    
     # 최근에 매매된 기록이 있는지 확인(포맷이 달라짐)
     try:
         whether_deal_recently = driver.find_element_by_class_name("date")
-        whether_deal_recently = True
-    except:
-        whether_deal_recently = False
-
-    if whether_deal_recently:
+        # whether_deal_recently = True
         household_info = driver.find_elements_by_class_name("data")[3].text
         built_year = driver.find_elements_by_class_name("data")[6].text
-    else:
+    except:
+        # whether_deal_recently = False
         household_info = driver.find_elements_by_class_name("data")[2].text
         built_year = driver.find_elements_by_class_name("data")[5].text
-
+    
     # 크롤링 된 내용 가공
     household_info_split = household_info.split()
     if len(household_info_split) == 1:
