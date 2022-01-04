a4d280749e5ccd3cb31de61c69dfdbcf2ad901fc
YubeenSon <65553480+YubeenSon@users.noreply.github.com>
2022-01-04 15:38:49 +0900

    flask localhost

---
+++ server/__init__.py
@@ -0,0 +1,8 @@
+# run.py에 있는 from app import app 을 통해 실행되는 모든 파일들을 
+# 전체적으로 초기화 및 실행을 위한 파일
+
+from flask import Flask
+from server.main.index import main as main
+
+server = Flask(__name__)
+server.register_blueprint(main)
