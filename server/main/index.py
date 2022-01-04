d1c1e764cf354908a2ba2c4eb6667a4f6c644d83
YubeenSon <65553480+YubeenSon@users.noreply.github.com>
2022-01-04 17:30:41 +0900

    add default page and post object value

--- server/main/index.py
+++ server/main/index.py
@@ -2,8 +2,13 @@ from flask import Blueprint, request, render_template, flash, redirect, url_for
 from flask import current_app as app
 
 main= Blueprint('main', __name__, url_prefix='/')
- 
+
+@main.route('/', methods=['GET'])
+def home():
+    return render_template('/default/index.html')
+
 @main.route('/main', methods=['GET'])
 def index():
+    test_data = 'test data array'
       # /main/index.html은 사실 /project_name/app/templates/main/index.html을 가리킵니다.
-      return render_template('/main/index.html')
+    return render_template('/main/index.html', test_data_html = test_data)
