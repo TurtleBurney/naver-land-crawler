f295ecf1441ada2094bfc67600317a1da9c09201
YubeenSon <65553480+YubeenSon@users.noreply.github.com>
2022-01-16 17:12:15 +0900

    clear contract tickles

--- models/__init__.py
+++ models/__init__.py
@@ -2,13 +2,12 @@ from .base import Base, BaseModel
 from .building import Building
 from .areatype import AreaType
 from .household import Household
-from .contract import Contract
-
+from .price import Price
 __all__ = [
     Base,
     BaseModel,
     Building,
     AreaType,
     Household,
-    Contract,
+    Price,
 ]
