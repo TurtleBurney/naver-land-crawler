b2d098226f53ba27b3dca4641efa84e8b9c6b703
YubeenSon <65553480+YubeenSon@users.noreply.github.com>
2022-01-31 16:01:17 +0900

    update flask starting method_flask run

--- run.py
+++
@@ -1,3 +0,0 @@
-from server import server
-
-server.run(host="0.0.0.0", port=80)
--- server/__init__.py
+++ server/__init__.py
@@ -1,6 +1,21 @@
 from flask import Flask
-
-server = Flask(__name__)
-
 from server.main.index import main as main
-server.register_blueprint(main)
+# app = Flask(__name__)
+
+"""
+어플리케이션 팩토리
+app 객체를 전역으로 사용할 때 발생하는 순환 참조 문제를 예방
+https://wikidocs.net/81504
+"""
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def hello_world():
+        return 'hello world!'
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.register_blueprint(main) # 이건 왜 안 돼..?
