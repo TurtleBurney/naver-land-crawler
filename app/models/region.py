160cd5de0ad8d907255d60fc42e96ba9bae06572
YubeenSon <65553480+YubeenSon@users.noreply.github.com>
2022-02-28 23:57:55 +0900

    create : region model

---
+++ app/models/region.py
@@ -0,0 +1,8 @@
+from app import db
+
+class Region(db.Model):
+    region_code = db.Column(db.String(50), primary_key=True)
+    city = db.Column(db.String(10), nullable=False)
+    gu = db.Column(db.String(10), nullable=True)
+    dong = db.Column(db.String(10), nullable=True)
+    parent_region_code = db.Column(db.String(50), nullable=True)
