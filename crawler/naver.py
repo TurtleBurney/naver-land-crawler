9e1c811fcbf524a494f9eb26091434b88500b97e
YubeenSon <65553480+YubeenSon@users.noreply.github.com>
2022-03-03 17:59:33 +0900

    add : add tag filtering function

--- crawler/naver.py
+++ crawler/naver.py
@@ -45,11 +45,20 @@ class NaverLandCrawler:
         url = f"{self.baseURL}/info/{building_id}?tradTpCd=A1:B1:B2&ajax=y"
         return url
 
+    def filter_building_info_tags(self, soup):
+        prices = soup.find_all("em", "txt_price")
+        floors = soup.find_all("span", "data")
+        title = soup.find("strong", "detail_complex_title")
+        details = soup.find_all("span", "detail_data_item")
+        tags = [prices, floors, title, details]
+        return tags
+
+    
     def filter_building_detail(self, soup) -> None:
         building_name = soup.find("strong", "detail_complex_title").text
         total_household = soup.find_all("span", "detail_data_item")[0].text[:-2]
-        lowest_floor = soup.find_all("span", "data")[1].text
-        highest_floor = soup.find_all("span", "data")[1].text
+        # lowest_floor = soup.find_all("span", "data")[1].text
+        # highest_floor = soup.find_all("span", "data")[1].text
         approval_date = soup.find_all("span", "detail_data_item")[2].text
         total_dong = soup.find_all("span", "detail_data_item")[1].text
         # number_address = 
@@ -57,7 +66,7 @@ class NaverLandCrawler:
         total_deal = soup.find_all("em", "txt_price")[0].text
         total_jeonse = soup.find_all("em", "txt_price")[1].text
         total_wolse = soup.find_all("em", "txt_price")[2].text
-        print(building_name, total_household, lowest_floor, highest_floor, approval_date, total_deal, sep="\n")
+        print(building_name, total_household, lowest_floor, total_dong, approval_date, total_deal, sep="\n")
 
 
 if __name__ == "__main__":
