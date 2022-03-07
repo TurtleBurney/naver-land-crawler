fc551affaa528c5d883ae82337acee462f804915
YubeenSon <65553480+YubeenSon@users.noreply.github.com>
2022-03-08 01:53:48 +0900

    modify : apply isort, black

--- crawler/object/building.py
+++ crawler/object/building.py
@@ -1,4 +1,4 @@
-class Building():
+class Building:
     def __init__(self, data: dict):
         self.building_name = data["title"]["title"]
 
@@ -15,32 +15,25 @@ class Building():
         self.total_deal = data["prices"]["deal"]
         self.total_jeonse = data["prices"]["jeonse"]
         self.total_wolse = data["prices"]["wolse"]
-   
 
     def get_info(self) -> dict:
         return {
-            "building_name" : self.building_name,
-
-            "total_household" : self.total_household,
-            "total_dong" : self.total_dong,
-            "approval_date" : self.approval_date,
-
-            "lowest_floor" : self.lowest_floor,
-            "highest_floor" : self.highest_floor,
-
-            "land_address" : self.land_address,
-            "road_address" : self.road_address,
-
-            "total_deal" : self.total_deal,
-            "total_jeonse" : self.total_jeonse,
-            "total_wolse" : self.total_wolse
+            "building_name": self.building_name,
+            "total_household": self.total_household,
+            "total_dong": self.total_dong,
+            "approval_date": self.approval_date,
+            "lowest_floor": self.lowest_floor,
+            "highest_floor": self.highest_floor,
+            "land_address": self.land_address,
+            "road_address": self.road_address,
+            "total_deal": self.total_deal,
+            "total_jeonse": self.total_jeonse,
+            "total_wolse": self.total_wolse,
         }
 
     def get_price_info(self) -> dict:
         return {
-            "total_deal" : self.total_deal,
-            "total_jeonse" : self.total_jeonse,
-            "total_wolse" : self.total_wolse
+            "total_deal": self.total_deal,
+            "total_jeonse": self.total_jeonse,
+            "total_wolse": self.total_wolse,
         }
-    
-
--- crawler/object/refiner.py
+++ crawler/object/refiner.py
@@ -1,65 +1,63 @@
 from bs4 import BeautifulSoup
 
-class Refiner():
+
+class Refiner:
     def __init__(self, html: str):
         tags = self.parse_html(html)
 
         self.refine_title(tags["title"])
         self.refine_detail(tags["details"])
         self.refine_floor(tags["floors"][1].text)
-        self.refine_address(tags["addresses"][2].text.split("addr")[1].split("realPriceInfoYn")[0])
+        self.refine_address(
+            tags["addresses"][2].text.split("addr")[1].split("realPriceInfoYn")[0]
+        )
         self.refine_price(tags["prices"])
 
     def parse_html(self, html: str) -> dict:
         soup = BeautifulSoup(html, "html.parser")
 
         tags = {
-            "title" : soup.find("strong", "detail_complex_title"),
-            "details" : soup.find_all("span", "detail_data_item"),
-            "floors" : soup.find_all("span", "data"),
-            "addresses" : soup.find_all('script', type='text/javascript'),
-            "prices" : soup.find_all("em", "txt_price")
+            "title": soup.find("strong", "detail_complex_title"),
+            "details": soup.find_all("span", "detail_data_item"),
+            "floors": soup.find_all("span", "data"),
+            "addresses": soup.find_all("script", type="text/javascript"),
+            "prices": soup.find_all("em", "txt_price"),
         }
         return tags
 
     def refine_title(self, tags: dict):
-        self.title = {
-            "title" : tags["title"].text
-        }
+        self.title = {"title": tags["title"].text}
 
     def refine_detail(self, tags: dict):
         self.detail = {
-            "total_household" : tags["details"][0].text[:-2],
-            "total_dong" : tags["details"][1].text,
-            "approval_date" : tags["details"][2].text
+            "total_household": tags["details"][0].text[:-2],
+            "total_dong": tags["details"][1].text,
+            "approval_date": tags["details"][2].text,
         }
 
     def refine_floor(self, tags: dict):
         splitted_floor = tags.split("/")
-        self.floor = {
-            "low" : splitted_floor[0],
-            "high" : splitted_floor[1][:-1]
-        }
+        self.floor = {"low": splitted_floor[0], "high": splitted_floor[1][:-1]}
 
     def refine_address(self, tags: dict):
         splitted_address = tags.split(",")
         self.address = {
-            "land" : splitted_address[0][4:-2],
-            "road" : splitted_address[-2].split("'")[1]
+            "land": splitted_address[0][4:-2],
+            "road": splitted_address[-2].split("'")[1],
         }
 
     def refine_price(self, tags: dict):
         self.price = {
-            "deal" : tags["prices"][0].text,
-            "jeonse" : tags["prices"][1].text,
-            "wolse" : tags["prices"][2].text
+            "deal": tags["prices"][0].text,
+            "jeonse": tags["prices"][1].text,
+            "wolse": tags["prices"][2].text,
         }
 
     def get_refined_data(self) -> dict:
         return {
-            "title" : self.title,
-            "details" : self.detail,
-            "floors" : self.floor,
-            "addresses" : self.address,
-            "prices" : self.price
+            "title": self.title,
+            "details": self.detail,
+            "floors": self.floor,
+            "addresses": self.address,
+            "prices": self.price,
         }
