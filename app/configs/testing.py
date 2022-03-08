8218cd3c4a8f3a96d755cea6f7f2f3c586db492e
handal <handal__@naver.com>
2022-03-08 21:11:20 +0900

    create : add testing configuration

---
+++ app/configs/testing.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+
+BASE_DIR = os.path.dirname(__file__)
+
+SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_TEST_URI")
+SECRET_KEY = os.getenv("SECRET_KEY")
