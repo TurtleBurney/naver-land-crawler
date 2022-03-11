5f9ba497558d7a97e93c92629543bdb5f517a6f9
YubeenSon <65553480+YubeenSon@users.noreply.github.com>
2022-03-12 02:23:59 +0900

    delete : delete cascade option

--- database/models/region.py
+++ database/models/region.py
@@ -3,13 +3,12 @@ from app import db
 
 class Region(db.Model):
     region_code = db.Column(db.String(50), primary_key=True)
+
     city = db.Column(db.String(10), nullable=False)
     gu = db.Column(db.String(10), nullable=True)
     dong = db.Column(db.String(10), nullable=True)
+    
     parent_region_code = db.Column(db.String(50), nullable=True)
 
     # Back Reference
-    buildings = db.relationship(
-        "Building", backref=db.backref("region", cascade="all, delete-orphan")
-    )
-    
+    buildings = db.relationship("Building", backref=db.backref("region"))
