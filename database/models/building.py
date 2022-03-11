fc1dce4607533c39d8444dc73a95b9a9e218b8a8
YubeenSon <65553480+YubeenSon@users.noreply.github.com>
2022-03-12 02:24:36 +0900

    modify : edit column name

--- database/models/building.py
+++ database/models/building.py
@@ -6,21 +6,22 @@ class Building(db.Model):
 
     building_name = db.Column(db.String(50), nullable=False)
     building_type = db.Column(
-        db.Enum("A1", "B1", "B2", name="building_code_enum"), nullable=False
+        db.Enum("APT", "OPT", name="building_type_enum"), nullable=False
     )
-    total_household = db.Column(db.Integer, nullable=False)
+
+    dong_count = db.Column(db.Integer, nullable=False)
+    household_count = db.Column(db.Integer, nullable=False)
     approval_date = db.Column(db.Date, nullable=False)
-    total_dong = db.Column(db.Integer, nullable=False)
 
     lowest_floor = db.Column(db.Integer, nullable=False)
     highest_floor = db.Column(db.Integer, nullable=False)
 
-    land_address = db.Column(db.String(100), nullable=False)  
+    land_address = db.Column(db.String(100), nullable=False)
     road_address = db.Column(db.String(100), nullable=False)
 
-    total_deal = db.Column(db.Integer, nullable=False)
-    total_jeonse = db.Column(db.Integer, nullable=False)
-    total_wolse = db.Column(db.Integer, nullable=False)
+    deal_count = db.Column(db.Integer, default=0)
+    jeonse_count = db.Column(db.Integer, default=0)
+    wolse_count = db.Column(db.Integer, default=0)
 
     # Foreign Key
     fk_region_code = db.Column(
