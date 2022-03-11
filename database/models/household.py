91d5d8f3755afb902b01231ae8bd1bb5d449a4ea
YubeenSon <65553480+YubeenSon@users.noreply.github.com>
2022-03-12 00:09:56 +0900

    modify : rename columns

--- database/models/household.py
+++ database/models/household.py
@@ -4,11 +4,12 @@ from sqlalchemy.sql import func
 
 class Household(db.Model):
     household_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    dong = db.Column(db.Integer, nullable=False)
+    building_dong = db.Column(db.String(10), nullable=False)
     floor = db.Column(db.Integer, nullable=False)
     direction = db.Column(db.String(10), nullable=False)
-    area = db.Column(db.Float, nullable=False)
-    link = db.Column(db.String(200), nullable=False)
+    supply_area = db.Column(db.Float, nullable=False)
+    private_area = db.Column(db.Float, nullable=False)
+    household_url = db.Column(db.String(200), nullable=False)
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
     updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
 
--- database/models/price.py
+++ database/models/price.py
@@ -1,16 +1,14 @@
 from app import db
 
-# from sqlalchemy.dialects.postgresql import ENUM
-
 
 class Price(db.Model):
     price_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     sale_type = db.Column(
         db.Enum("Deal", "Jeonse", "Wolse", name="sale_type_enum"), nullable=False
     )
-    default_price = db.Column(db.Integer, nullable=False)
-    highest_price = db.Column(db.Integer, nullable=True)
-    wolse_price = db.Column(db.Integer, nullable=True)
+    min_price = db.Column(db.String(50), nullable=False)
+    max_price = db.Column(db.String(50), nullable=False)
+    wolse_price = db.Column(db.String(50), nullable=True)
 
     # Foreign Key
     fk_house_id = db.Column(
