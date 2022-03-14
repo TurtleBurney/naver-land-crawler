8f7c98f91fd550c0201e4baff441426bce6795de
YubeenSon <65553480+YubeenSon@users.noreply.github.com>
2022-03-14 16:53:47 +0900

    modify : change enum to UPPER case and fk name

--- database/models/contract_price.py
+++ database/models/contract_price.py
@@ -4,7 +4,7 @@ from . import db
 class ContractPrice(db.Model):
     contract_price_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     contract_type = db.Column(
-        db.Enum("Deal", "Jeonse", "Wolse", name="contract_type_enum"), nullable=False
+        db.Enum("DEAL", "JEONSE", "WOLSE", name="contract_type_enum"), nullable=False
     )
     
     # 단위 : 만 원
@@ -13,6 +13,6 @@ class ContractPrice(db.Model):
     wolse_price = db.Column(db.String(10), nullable=True)
 
     # Foreign Key
-    fk_house_id = db.Column(
+    fk_household_id = db.Column(
         db.Integer, db.ForeignKey("household.household_id", ondelete="CASCADE")
     )
