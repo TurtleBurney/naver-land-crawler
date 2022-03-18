b00fca625fd14a21c75a0fad7fd675a526c6949d
YubeenSon <65553480+YubeenSon@users.noreply.github.com>
2022-03-18 16:12:37 +0900

    modify : edit parent_code formatter

--- libs/region/region.py
+++ libs/region/region.py
@@ -1,10 +1,10 @@
 class Region:
     def __init__(self, code, city, gu=None, dong=None, parent_code=None):
-        self.code = code
+        self.code = self.encode_region(code)
         self.city = city
         self.gu = gu
         self.dong = dong
-        self.parent_code = parent_code
+        self.parent_code = self.process_parent_code(parent_code)
 
     def get(self):
         return {
@@ -14,3 +14,12 @@ class Region:
             "dong": self.dong,
             "parent_code": self.parent_code,
         }
+
+    def process_parent_code(self, parent_code):
+        if parent_code is None:
+            return parent_code
+        else:
+            return self.encode_region(parent_code)
+
+    def encode_region(self, code):
+        return code.ljust(10, "0")
--- libs/region/region_parser.py
+++ libs/region/region_parser.py
@@ -36,10 +36,7 @@ class RegionParser:
         return region_data
 
     def get_region_code(self, region_data: dict):
-        region_codes = list(region_data.keys())
-
-        for code in region_codes:
-            address = region_data[code]
+        for code, address in region_data.items():
             splitted_address = address.split(" ")
 
             # 시/도 단위
@@ -65,12 +62,11 @@ class RegionParser:
 
     def get_sejong_data(self, code: list, splitted_address: list):
         # ex) 세종특별자치시 소정면, 세종특별자치시 반곡동
-        parent_code = code[:5] + "00000"
         region = Region(
             code,
             city=splitted_address[0],
             dong=splitted_address[1],
-            parent_code=parent_code,
+            parent_code=code[:5],
         )
         self.regions.append(region.get())
 
@@ -79,16 +75,12 @@ class RegionParser:
             pass
 
         elif len(splitted_address) == 3:
-            parent_code = code[:2] + "00000000"
             gu = splitted_address[1] + " " + splitted_address[2]
 
-            region = Region(
-                code, city=splitted_address[0], gu=gu, parent_code=parent_code
-            )
+            region = Region(code, city=splitted_address[0], gu=gu, parent_code=code[:2])
             self.regions.append(region.get())
 
         else:
-            parent_code = code[:4] + "000000"
             gu = splitted_address[1] + " " + splitted_address[2]
 
             region = Region(
@@ -96,33 +88,29 @@ class RegionParser:
                 city=splitted_address[0],
                 gu=gu,
                 dong=splitted_address[3],
-                parent_code=parent_code,
+                parent_code=code[:4],
             )
             self.regions.append(region.get())
 
     def get_gudong_data(self, code, splitted_address):
         # 구/군 단위
         if len(splitted_address) == 2:
-            parent_code = code[:2] + "00000000"
-
             region = Region(
                 code,
                 city=splitted_address[0],
                 gu=splitted_address[1],
-                parent_code=parent_code,
+                parent_code=code[:2],
             )
             self.regions.append(region.get())
 
         # 동/읍/면 단위
         elif len(splitted_address) == 3:
-            parent_code = code[:5] + "00000"
-
             region = Region(
                 code,
                 city=splitted_address[0],
                 gu=splitted_address[1],
                 dong=splitted_address[2],
-                parent_code=parent_code,
+                parent_code=code[:5],
             )
             self.regions.append(region.get())
 
