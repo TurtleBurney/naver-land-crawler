b44d0d9a5a347e21070e1ff09efeb29a1984c7d0
YubeenSon <65553480+YubeenSon@users.noreply.github.com>
2022-03-20 23:47:40 +0900

    modify : modify parser to module not main

--- libs/region/region_parser.py
+++ libs/region/region_parser.py
@@ -9,53 +9,28 @@ DONGEUP = 2
 
 class RegionParser:
     def __init__(self):
+        self.input_file_path = "./data/region_code_name.json"
+        self.output_file_path = "test_2.json"
         self.regions = []
 
     def run(self):
-        file_path = "./data/region_code_name.json"
-        region_data = RegionExtractor(file_path).run()
-        self.get_region_code(region_data)
-        with open("test_2.json", "w", encoding="utf-8") as file:
-            file.write(json.dumps(self.regions, ensure_ascii=False, indent="\t"))
+        region_data = RegionExtractor(self.input_file_path).run()
 
-    def get_region_code(self, region_data: dict):
+        regions = self.parse_region(region_data)
+
+        self.save_region_to_json(regions)
+        self.regions = regions
+
+    def parse_region(self, region_data: dict):
+        regions = []
         for code, address in region_data.items():
             region_names = address.split(" ")
 
-            # 시/도 단위
-            if len(region_names) == 1:
-                self.get_level1_data(code, region_names)
+            region = Region(code, region_names).get()
 
-            # 시도 + 시구군
-            elif len(region_names) == 2:
-                self.get_level2_data(code, region_names)
+            regions.append(region)
+        return regions
 
-            # 시도 + 시구군 + 동읍면
-            elif len(region_names) == 3:
-                self.get_level3_data(code, region_names)
-
-    def get_level1_data(self, code, region_names: list):
-        region = Region(code, city=region_names[SIDO])
-        self.regions.append(region.get())
-
-    def get_level2_data(self, code, region_names: list):
-        region = Region(
-            code,
-            city=region_names[SIDO],
-            gu=region_names[SIGUGUN],
-        )
-        self.regions.append(region.get())
-
-    def get_level3_data(self, code, region_names: list):
-        region = Region(
-            code,
-            city=region_names[SIDO],
-            gu=region_names[SIGUGUN],
-            dong=region_names[DONGEUP],
-        )
-        self.regions.append(region.get())
-
-
-if __name__ == "__main__":
-    region_parser = RegionParser()
-    region_parser.run()
+    def save_region_to_json(self, regions):
+        with open(self.output_file_path, "w", encoding="utf-8") as file:
+            file.write(json.dumps(regions, ensure_ascii=False, indent="\t"))
