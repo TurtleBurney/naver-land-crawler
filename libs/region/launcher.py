6e549a2bfddde41f14141f4208c1b14a8e13c882
YubeenSon <65553480+YubeenSon@users.noreply.github.com>
2022-03-21 23:30:46 +0900

    add : add storing logic to launcher and path

--- libs/region/launcher.py
+++ libs/region/launcher.py
@@ -1,9 +1,21 @@
+import sys, os
+
+sys.path.append(
+    os.path.dirname(
+        os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+    )
+)
+
 from region_parser import RegionParser
+from db_store import DbStorer
+from database.models.region import Region
 
 
 def run():
     region_parser = RegionParser()
     region_parser.run()
+    db_store = DbStorer(Region, region_parser.regions)
+    db_store.run()
 
 
 if __name__ == "__main__":
