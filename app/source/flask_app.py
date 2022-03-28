8c503a4c15ed5f4d2708011104daace49ca9063f
YubeenSon <65553480+YubeenSon@users.noreply.github.com>
2022-03-28 10:43:48 +0900

    create : add run file at root dir

--- app/source/flask_app.py
+++ app/source/flask_app.py
@@ -5,7 +5,7 @@ from app.configs import config
 
 
 def create_app(cfg=None):
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="../templates", static_folder="../static")
     app.config.from_object(config)
 
     if cfg:
@@ -28,4 +28,4 @@ def register_blueprints(app):
 
 
 if __name__ == "__main__":
-    create_app()
+    app = create_app()
