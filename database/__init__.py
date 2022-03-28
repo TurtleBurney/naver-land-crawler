75abdb4494e23eeeb13c50a6c27a76c5a3da7e76
YubeenSon <65553480+YubeenSon@users.noreply.github.com>
2022-03-28 10:42:51 +0900

    modify : edit dir

--- database/__init__.py
+++ database/__init__.py
@@ -1,9 +1,10 @@
+import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 
 load_dotenv(verbose=True)
 
-engine = create_engine("SQLALCHEMY_DATABASE_URI")
+engine = create_engine(os.getenv("SQLALCHEMY_DATABASE_URI"))
 
 from sqlalchemy.orm import sessionmaker
 
