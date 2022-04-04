87849a006d5982874423ae9ea7654e7231117207
YubeenSon <65553480+YubeenSon@users.noreply.github.com>
2022-04-04 18:45:03 +0900

    add : complete parsing deal type households

--- naver_land/crawl_household.py
+++ naver_land/crawl_household.py
@@ -1,5 +1,5 @@
 import json
-
+from pprint import pprint
 from base_crawler import BaseCrawler
 from object import Building, Household
 
@@ -25,15 +25,34 @@ class HouseholdCrawler(BaseCrawler):
         for page_num in range(1, total_page_num + 1):
             url = self.household_list_url(sample_sale_type, page_num)
             response = self.get_request(url)
+
+            self.save_household_info(response.json(), page_num)
+
             household_info = self.parse_household_info(response.json(), page_num)
+            household_list += household_info
         return household_list
 
     def parse_household_info(self, response: json, page_num):
         household_raw_list = response["result"]["list"]
-        household_data = []
-        for i in range(20):
-            household = household_raw_list[0]
-            # TODO : 개별 매물 household같은 클래스로 담아 저장하기
+        household_info = []
+
+        iter_count = 20
+        if page_num == int(self.contract_cnt["deal_count"]) // 20 + 1:
+            iter_count = int(self.contract_cnt["deal_count"]) % 20
+
+        for i in range(iter_count):
+            household_raw = household_raw_list[i]
+
+            household_data = {
+                "building_dong": household_raw["bildNm"][:-1],
+                "floor": household_raw["flrInfo"],
+                "direction": household_raw["direction"],
+                "supply_area": household_raw["spc1"],
+                "private_area": household_raw["spc2"],
+            }
+            household = Household(household_data, self.building_code)
+            household_info.append(household)
+        return household_info
 
     def save_household_info(self, response: json, page_num):
         with open(f"./temp_household_{page_num}.json", "w", encoding="utf-8") as file:
