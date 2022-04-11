5e266a1c2acd8884957b48a89f313e1520780aae
YubeenSon <65553480+YubeenSon@users.noreply.github.com>
2022-04-11 15:51:51 +0900

    delete : delete max, min wolse

--- naver_land/object/contract_price.py
+++ naver_land/object/contract_price.py
@@ -5,13 +5,10 @@ class ContractPrice:
         self.min_price = data["min_price"]
         self.max_price = data["max_price"]
 
-        self.min_wolse_price = data["min_wolse_price"]
-        self.max_wolse_price = data["max_wolse_price"]
-
         self.fk_household = data["fk_household"]
 
     def __str__(self) -> str:
-        return f"<fk.household : {self.fk_household}, contract_type : {self.contract_type}, min_price : {self.min_price}/{self.min_wolse_price}, max_price : {self.max_price}/{self.max_wolse_price}>"
+        return f"<fk.household : {self.fk_household}, contract_type : {self.contract_type}, min_price : {self.min_price}, max_price : {self.max_price}>"
 
     def __repr__(self) -> str:
-        return f"<fk.household : {self.fk_household}, contract_type : {self.contract_type}, min_price : {self.min_price}/{self.min_wolse_price}, max_price : {self.max_price}/{self.max_wolse_price}>"
+        return f"<fk.household : {self.fk_household}, contract_type : {self.contract_type}, min_price : {self.min_price}, max_price : {self.max_price}>"
