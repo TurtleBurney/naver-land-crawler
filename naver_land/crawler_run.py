e3bb3412937a60577eb2ab64f333e945d77167c7
YubeenSon <65553480+YubeenSon@users.noreply.github.com>
2022-04-13 09:55:25 +0900

    add : crawl price at crawler_run

--- naver_land/crawler_run.py
+++ naver_land/crawler_run.py
@@ -2,16 +2,14 @@
 naver crawler
 
 """
-from crawler import (
-    BuildingCrawler,
-    ContractPriceCrawler,
-    HouseholdCrawler,
-    HouseholdPriceCrawler,
-)
 import time
+from pprint import pprint
+
+from crawler import BuildingCrawler, HouseholdCrawler, SaleOfferCrawler
 from logger import init_logger
 
 logger = init_logger()
+
 sale_type_enum = {"deal": "A1", "jeonse": "B1", "wolse": "B2"}
 
 
@@ -29,13 +27,11 @@ class NaverLandCrawler:
         sample_region_code = "1141011000"
 
         households = []
-        contract_prices = []
+        sale_offers = []
 
         household_cralwer = HouseholdCrawler()
-        contract_price_crawler = ContractPriceCrawler()
-        householdPriceResponse = HouseholdPriceCrawler()
+        sale_offer_crawler = SaleOfferCrawler()
 
-        # TODO : 반복문으로 building_id마다 크롤링
         building_crawler = BuildingCrawler()
         building_crawler.set_region(sample_region_code)
         buildings = building_crawler.crawl()
@@ -43,35 +39,25 @@ class NaverLandCrawler:
         for building in buildings:
             building_code = building.building_code
 
-            householdPriceResponse.set_building_info(building)
+            household_cralwer.set_building_info(sample_region_code, building_code)
             household_count = 0
             for sale_type in sale_type_enum:
-                householdPriceResponse.set_sale_type(sale_type)
+                household_cralwer.set_sale_type(sale_type)
                 household_page_num = building.get_household_page_num(sale_type)
 
                 for page_idx in range(household_page_num):
                     page_index = page_idx + 1
 
-                    response = householdPriceResponse.crawl(page_index)
+                    household_list = household_cralwer.crawl(page_index)
+                    households += household_list
 
-                    # Household From response
-                    raw_response_list = response["result"]["list"]
+                    for household in household_list:
+                        sale_offer_crawler.set_household(household.household_code)
 
-                    for raw_response in raw_response_list:
-                        household_data = household_cralwer.refine(raw_response)
-                        household = household_cralwer.get_household(
-                            household_data, building_code
-                        )
-                        households.append(household)
-                        household_count += 1
+                        sale_offer_list = sale_offer_crawler.crawl()
+                        sale_offers += sale_offer_list
+                        pprint(sale_offer_list)
 
-                        contract_price_data = contract_price_crawler.refine(
-                            raw_response
-                        )
-                        contract_price = contract_price_crawler.get_contract_price(
-                            contract_price_data
-                        )
-                        contract_prices.append(contract_price)
             logger.info(
                 f"building {building_code} Crawled >> household_count : {household_count}"
             )
