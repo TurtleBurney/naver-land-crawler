d721f5729a7813f3ec5fd5f7ee23275773f40fad
YubeenSon <65553480+YubeenSon@users.noreply.github.com>
2022-04-13 09:49:25 +0900

    modify : change contract_price to sale_offer

--- naver_land/crawler/__init__.py
+++ naver_land/crawler/__init__.py
@@ -1,13 +1,13 @@
 from crawler.base import BaseCrawler
 from crawler.building import BuildingCrawler
-from crawler.contract_price import ContractPriceCrawler
 from crawler.household import HouseholdCrawler
 from crawler.household_price_response import HouseholdPriceCrawler
+from crawler.sale_offer import SaleOfferCrawler
 
 __all__ = [
     BaseCrawler,
     BuildingCrawler,
-    ContractPriceCrawler,
     HouseholdCrawler,
     HouseholdPriceCrawler,
+    SaleOfferCrawler,
 ]
