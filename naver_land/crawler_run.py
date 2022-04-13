eb8f92355da063210cbd825b9a17e5eb5cb0a50e
YubeenSon <65553480+YubeenSon@users.noreply.github.com>
2022-04-13 10:56:21 +0900

    modify : split run function to detail

--- naver_land/crawler_run.py
+++ naver_land/crawler_run.py
@@ -6,6 +6,7 @@ import time
 from pprint import pprint
 
 from crawler import BuildingCrawler, HouseholdCrawler, SaleOfferCrawler
+from object import Building
 from logger import init_logger
 
 logger = init_logger()
@@ -29,40 +30,78 @@ class NaverLandCrawler:
         households = []
         sale_offers = []
 
-        household_cralwer = HouseholdCrawler()
+        household_crawler = HouseholdCrawler()
         sale_offer_crawler = SaleOfferCrawler()
 
         building_crawler = BuildingCrawler()
-        building_crawler.set_region(sample_region_code)
-        buildings = building_crawler.crawl()
+        buildings = self.crawl_building(building_crawler, sample_region_code)
+        time.sleep(1)
 
         for building in buildings:
-            building_code = building.building_code
-
-            household_cralwer.set_building_info(sample_region_code, building_code)
             household_count = 0
-            for sale_type in sale_type_enum:
-                household_cralwer.set_sale_type(sale_type)
-                household_page_num = building.get_household_page_num(sale_type)
 
-                for page_idx in range(household_page_num):
-                    page_index = page_idx + 1
+            household_list = self.crawl_household(
+                household_crawler, building, sample_region_code
+            )
 
-                    household_list = household_cralwer.crawl(page_index)
-                    households += household_list
+            households += household_list
+            household_count += len(household_list)
+            time.sleep(1)
 
-                    for household in household_list:
-                        sale_offer_crawler.set_household(household.household_code)
+            for household in household_list:
+                household_code = household.household_code
+                # TODO : Cralwer도 param으로 넘기는게 맞는가 확인
+                sale_offer_list = self.crawl_sale_offer(
+                    sale_offer_crawler, household_code
+                )
 
-                        sale_offer_list = sale_offer_crawler.crawl()
-                        sale_offers += sale_offer_list
-                        pprint(sale_offer_list)
+                sale_offers += sale_offer_list
+                pprint(sale_offer_list)
 
             logger.info(
-                f"building {building_code} Crawled >> household_count : {household_count}"
+                f"building {building.building_code} Crawled >> household_count : {household_count}"
             )
             time.sleep(3)
         # TODO : Building 정보 DB에 넣기
+
+    def crawl_building(
+        self, building_crawler: BuildingCrawler, region_code: str
+    ) -> list:
+        building_crawler.set_region(region_code)
+        buildings = building_crawler.crawl()
+
+        return buildings
+
+    def crawl_household(
+        self,
+        household_crawler: HouseholdCrawler,
+        building: Building,
+        region_code: str,
+    ):
+        building_code = building.building_code
+
+        household_crawler.set_building_info(region_code, building_code)
+        households_in_building = []
+
+        for sale_type in sale_type_enum:
+            household_crawler.set_sale_type(sale_type)
+            household_page_num = building.get_household_page_num(sale_type)
+
+            for page_idx in range(household_page_num):
+                page_index = page_idx + 1
+
+                household_list = household_crawler.crawl(page_index)
+                households_in_building += household_list
+
+        return households_in_building
+
+    def crawl_sale_offer(
+        self, sale_offer_crawler: SaleOfferCrawler, household_code: str
+    ) -> list:
+        sale_offer_crawler.set_household(household_code)
+
+        sale_offer_list = sale_offer_crawler.crawl()
+        return sale_offer_list
 
 
 if __name__ == "__main__":
