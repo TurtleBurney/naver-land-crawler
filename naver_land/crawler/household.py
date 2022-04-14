4cef130507171eb29b6bd0e9506bf08313b08496
YubeenSon <65553480+YubeenSon@users.noreply.github.com>
2022-04-15 00:18:33 +0900

    modify : edit sale type to sale type code

--- naver_land/crawler/household.py
+++ naver_land/crawler/household.py
@@ -10,14 +10,20 @@ class HouseholdCrawler(BaseCrawler):
         self.region_code = None
         self.building_code = None
 
-        self.sale_type = None
+        self.sale_type_code = None
 
     def set_building_info(self, region_code: str, building_code: str) -> None:
         self.region_code = region_code
         self.building_code = building_code
 
     def set_sale_type(self, sale_type: str) -> None:
-        self.sale_type = sale_type_enum[sale_type]
+        sale_type_code = {
+            "deal": "A1",
+            "jeonse": "B1",
+            "wolse": "B2",
+        }[sale_type]
+
+        self.sale_type_code = sale_type_code
 
     def crawl(self, page_num: int) -> list:
         # Step 1
@@ -45,7 +51,9 @@ class HouseholdCrawler(BaseCrawler):
     def household_list_url(self, page_num: int) -> str:
         base_url = f"{self.naverURL}/complex/getComplexArticleList?"
         region_url = f"hscpNo={self.building_code}&cortarNo={self.region_code}"
-        detail_url = f"&tradTpCd={self.sale_type}&order=point_&showR0=N&page={page_num}"
+        detail_url = (
+            f"&tradTpCd={self.sale_type_code}&order=point_&showR0=N&page={page_num}"
+        )
 
         url = base_url + region_url + detail_url
 
